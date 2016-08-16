f1c4406fbe0613f9b88e481e011d2ad5e5818e07
Dylan McKay <dylanmckay34@gmail.com>
2016-08-16 16:36:59 +1200

    Do more error handling

--- src/io/error.rs
+++ src/io/error.rs
@@ -2,13 +2,22 @@ use std;
 
 pub enum Error
 {
+    BadData(String),
     Io(std::io::Error),
+    InvalidUtf16(std::string::FromUtf16Error),
 }
 
 impl From<std::io::Error> for Error
 {
     fn from(error: std::io::Error) -> Self {
         Error::Io(error)
+    }
+}
+
+impl From<std::string::FromUtf16Error> for Error
+{
+    fn from(error: std::string::FromUtf16Error) -> Self {
+        Error::InvalidUtf16(error)
     }
 }
 
--- src/io/types/string.rs
+++ src/io/types/string.rs
@@ -11,18 +11,16 @@ impl<'a> Type for String
         let mut str_bytes = Vec::new();
         buffer.take(byte_count as _).read_to_end(&mut str_bytes)?;
 
-        // FIXME: this shouldn't be a panic
-        assert_eq!(byte_count as usize, str_bytes.len());
+        if byte_count as usize != str_bytes.len() {
+            return Err(Error::BadData("string is shorter than prefix varint claims".to_owned()));
+        }
 
         let str_bytes: Vec<u16> = str_bytes.chunks(2).map(|a| {
             ((a[0] as u16) << 8) | ((a[1] as u16) << 0)
         }).collect();
 
-        match String::from_utf16(str_bytes.as_slice()) {
-            Ok(val) => Ok(val),
-            // FIXME: this shouldn't be a panic
-            Err(_) => panic!("invalid utf16"),
-        }
+        let str = String::from_utf16(str_bytes.as_slice())?;
+        Ok(str)
     }
 
     fn write(&self, buffer: &mut Buffer) -> Result<(), Error> {
