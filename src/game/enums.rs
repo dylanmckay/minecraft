185aef2cd6f5419fedb9fab66f3cdc6b5dda47f3
Dylan McKay <dylanmckay34@gmail.com>
2016-08-18 04:03:06 +1200

    Add 'enums' module

---
+++ src/game/enums.rs
@@ -0,0 +1,77 @@
+pub struct DecodingError;
+
+#[derive(Copy,Clone,Debug,PartialEq,Eq)]
+pub enum GameVariant
+{
+    Survival = 0,
+    Creative = 1,
+    Adventure = 2,
+    Spectator = 3,
+}
+
+#[derive(Copy,Clone,Debug,PartialEq,Eq)]
+pub struct GameMode
+{
+    pub variant: GameVariant,
+    pub is_hardcore: bool,
+}
+
+#[derive(Copy,Clone,Debug,PartialEq,Eq)]
+pub enum Dimension
+{
+    Nether = -1,
+    Overworld = 0,
+    End = 1,
+}
+
+impl GameVariant
+{
+    pub fn encode(self) -> u8 { self as u8 }
+    pub fn decode(val: u8) -> Result<Self, DecodingError> {
+        match val {
+            0 => Ok(GameVariant::Survival),
+            1 => Ok(GameVariant::Creative),
+            2 => Ok(GameVariant::Adventure),
+            3 => Ok(GameVariant::Spectator),
+            _ => Err(DecodingError),
+        }
+    }
+}
+
+impl GameMode
+{
+    pub fn encode(self) -> u8 {
+        let hardcore_bit = if self.is_hardcore { 0b1000 } else { 0 };
+        self.variant.encode() | hardcore_bit
+    }
+
+    pub fn decode(mut val: u8) -> Result<Self, DecodingError> {
+        let is_hardcore = if val & 0b1000 == 0b1000 {
+            // Remove the hardcore bit from the value.
+            val &= !0b1000;
+            true
+        } else {
+            false
+        };
+
+        if let Ok(variant) = GameVariant::decode(val) {
+            Ok(GameMode { variant: variant, is_hardcore: is_hardcore })
+        } else {
+            Err(DecodingError)
+        }
+    }
+}
+
+impl Dimension
+{
+    pub fn encode(self) -> i32 { self as i32 }
+    pub fn decode(val: i32) -> Result<Self, DecodingError> {
+        match val {
+            -1 => Ok(Dimension::Nether),
+            0 => Ok(Dimension::Overworld),
+            1 => Ok(Dimension::End),
+            _ => Err(DecodingError),
+        }
+    }
+}
+
--- src/game/mod.rs
+++ src/game/mod.rs
@@ -2,10 +2,12 @@ pub use self::client::Client;
 pub use self::entity::{Entity, EntityKind, Entities};
 pub use self::context::Context;
 pub use self::types::*;
+pub use self::enums::{GameVariant, GameMode};
 
 pub mod client;
 pub mod entity;
 pub mod context;
+pub mod enums;
 
 pub mod types;
 
