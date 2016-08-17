9ee7f60aa2c39affcd2664de70a4007da8da54a6
Dylan McKay <dylanmckay34@gmail.com>
2016-08-18 06:32:49 +1200

    [protocol] Implement the 'slot' type

--- src/protocol/error.rs
+++ src/protocol/error.rs
@@ -7,7 +7,8 @@ pub enum Error
     BadData(String),
     Io(std::io::Error),
     InvalidUtf8(std::string::FromUtf8Error),
-    UnknownPacket(protocol::packet::Data)
+    UnknownPacket(protocol::packet::Data),
+    Nbt(::nbt::Error),
 }
 
 impl From<std::io::Error> for Error
@@ -21,6 +22,13 @@ impl From<std::string::FromUtf8Error> for Error
 {
     fn from(error: std::string::FromUtf8Error) -> Self {
         Error::InvalidUtf8(error)
+    }
+}
+
+impl From<::nbt::Error> for Error
+{
+    fn from(e: ::nbt::Error) -> Self {
+        Error::Nbt(e)
     }
 }
 
--- src/protocol/types/slot.rs
+++ src/protocol/types/slot.rs
@@ -1,8 +1,10 @@
 use protocol::{Type, Error};
 use std::io::{Read, Write};
+use nbt;
 
 use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
 
+#[derive(Clone,Debug)]
 pub enum Slot
 {
     Empty,
@@ -10,7 +12,54 @@ pub enum Slot
         block_id: i16,
         item_count: i8,
         item_damage: i16,
-        // TODO: add NBT
+        nbt: Option<nbt::Blob>,
     },
+}
+
+impl Type for Slot
+{
+    fn read(read: &mut Read) -> Result<Self, Error> {
+        let block_id = read.read_i16::<BigEndian>()?;
+
+        if block_id == -1 {
+            Ok(Slot::Empty)
+        } else {
+            let item_count = read.read_i8()?;
+            let item_damage = read.read_i16::<BigEndian>()?;
+
+            let nbt = if block_id == 0 {
+                None
+            } else {
+                Some(nbt::Blob::from_reader(read)?)
+            };
+
+            Ok(Slot::Filled {
+                block_id: block_id,
+                item_count: item_count,
+                item_damage: item_damage,
+                nbt: nbt,
+            })
+        }
+
+    }
+
+    fn write(&self, write: &mut Write) -> Result<(), Error> {
+        match *self {
+            Slot::Empty => {
+                write.write_i16::<BigEndian>(-1)?;
+            },
+            Slot::Filled { block_id, item_count, item_damage, ref nbt } => {
+                write.write_i16::<BigEndian>(block_id)?;
+                write.write_i8(item_count)?;
+                write.write_i16::<BigEndian>(item_damage)?;
+
+                if let Some(ref nbt) = *nbt {
+                    nbt.write(write)?;
+                }
+            },
+        }
+
+        Ok(())
+    }
 }
 
