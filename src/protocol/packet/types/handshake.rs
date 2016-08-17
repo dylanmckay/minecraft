0400e571acd94c88f5a7ac7184c1d5b427e2c70a
Dylan McKay <dylanmckay34@gmail.com>
2016-08-18 00:01:36 +1200

    Add packet creation macro

--- src/protocol/packet/types/handshake.rs
+++ src/protocol/packet/types/handshake.rs
@@ -1,40 +1,12 @@
 use protocol::types::*;
-use protocol::{packet, Error};
-use std::io::{Read, Write};
 
 pub const STATE_STATUS: VarInt = VarInt(1);
 pub const STATE_LOGIN: VarInt = VarInt(2);
 
-#[derive(Clone, Debug)]
-pub struct Handshake
-{
-    pub protocol_version: VarInt,
-    pub server_address: String,
-    pub server_port: u16,
-    pub next_state: VarInt,
-}
-
-impl packet::Realization for Handshake
-{
-    const PACKET_ID: VarInt = VarInt(0x00);
-    const DESCRIPTION: &'static str = "handshake";
-
-    fn parse(read: &mut Read) -> Result<Self, Error> {
-        Ok(Handshake {
-            protocol_version: VarInt::read(read)?,
-            server_address: String::read(read)?,
-            server_port: u16::read(read)?,
-            next_state: VarInt::read(read)?,
-        })
-    }
-
-    fn write_payload(&self, write: &mut Write) -> Result<(), Error> {
-        self.protocol_version.write(write)?;
-        self.server_address.write(write)?;
-        self.server_port.write(write)?;
-        self.next_state.write(write)?;
-
-        Ok(())
-    }
-}
+define_packet!(0x00 => Handshake; [
+    protocol_version: VarInt,
+    server_address: String,
+    server_port: u16,
+    next_state: VarInt
+]);
 
--- src/protocol/packet/types/mod.rs
+++ src/protocol/packet/types/mod.rs
@@ -5,6 +5,44 @@ pub use self::encryption_request::EncryptionRequest;
 pub use self::encryption_response::EncryptionResponse;
 pub use self::set_compression::SetCompression;
 
+macro_rules! define_packet {
+    ($id:expr => $name:ident; [
+         $( $field:ident: $ty:ident),+ ]) => {
+        #[derive(Clone, Debug)]
+        pub struct $name
+        {
+            $(
+                pub $field: $ty,
+            )*
+        }
+
+        impl ::protocol::packet::Realization for $name
+        {
+            const PACKET_ID: ::protocol::types::VarInt = VarInt($id);
+            const DESCRIPTION: &'static str = stringify!($name);
+
+            fn parse(read: &mut ::std::io::Read)
+                -> Result<Self, ::protocol::Error> {
+                use ::protocol::types::*;
+
+                Ok($name {
+                    $(
+                        $field: $ty::read(read)?,
+                    )*
+                })
+            }
+
+            fn write_payload(&self, write: &mut ::std::io::Write)
+                -> Result<(), ::protocol::Error> {
+
+                $( self.$field.write(write)?; )*
+
+                Ok(())
+            }
+        }
+    }
+}
+
 pub mod handshake;
 pub mod login_start;
 pub mod login_success;
